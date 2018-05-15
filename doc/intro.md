03d705332ed38f63559a8adc6947a4155422cf88
Robert Hambrock <roberthambrock@gmail.com>
2018-05-15 23:57:14 +0200

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to caspojure
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/caspojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns caspojure.core-test
+  (:require [clojure.test :refer :all]
+            [caspojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
