7e83f5d39acc272d8c076581f4d065270829b72e
Robert Hambrock <roberthambrock@gmail.com>
2018-05-19 21:08:35 +0200

    Implementation altered to match pZ4's work
    Distinguish between vote-messages and relay-messages. Matches pZ4/p2pvoting,
    barring that we include a relayer's estimate in the message it relays.

--- src/caspojure/message.clj
+++ src/caspojure/message.clj
@@ -2,19 +2,26 @@
   (:require [clojure.spec.gen.alpha :as gen]
             [clojure.spec.alpha :as s]))
 
-(s/def ::vote #{0 1})
-(s/def ::estimate #{0 1})
-(s/def ::genesis-message (s/tuple ::vote ::estimate nil?))
-(s/def ::message (s/or :message (s/tuple ::vote ::estimate (s/every ::message :min-count 1 :gen-max 4))
+(def relayer-count 10)
+(s/def ::relayer-id (s/int-in 0 relayer-count))
+(s/def ::vote boolean?)
+(s/def ::vote-message (s/tuple ::relayer-id ::vote))
+(s/def ::estimate boolean?)
+(s/def ::genesis-message (s/tuple ::relayer-id ::estimate nil?))
+(s/def ::relay-message (s/or :message (s/tuple ::relayer-id ::estimate (s/every (s/or :relay ::relay-message :vote ::vote-message) :min-count 1 :gen-max 4))
                        :genesis ::genesis-message))
 
-(s/valid? ::message [1 0
-                     [[1 0
-                       [[0 0 nil]
-                        [1 0 [[0 0 nil]]]]]
-                      [0 0 nil]]
+(gen/generate (s/gen ::vote-message))
+(gen/generate (s/gen ::relay-message))
+
+(s/valid? ::vote-message [2 false])
+
+(s/explain ::relay-message [1 false
+                     [[9 true
+                       [[2 false nil]
+                        [1 true [[0 true nil]]]]]
+                      [0 true nil]]
                      ])
 
-(s/valid? ::message [1 0 nil])
-
-(gen/generate (s/gen ::message))
+(gen/generate (s/gen ::vote-message))
+(gen/generate (s/gen ::relay-message))
